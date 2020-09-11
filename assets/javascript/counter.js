1654a8517999fce509a9e88ef06b73a1b2c1b05b
svati08-Amazon <patelsv1822@gmail.com>
2020-09-11 17:30:58 -0400

    A Simple Counter

---
+++ assets/javascript/counter.js
@@ -0,0 +1,22 @@
+console.log("Hi Svati");
+
+let incrementButton = document.querySelector("#increment");
+let decrementButton = document.querySelector("#decrement");
+let counter = document.querySelector("#counter");
+
+incrementButton.addEventListener("click", function () {
+  console.log("+ button clicked");
+  let newCounterValue = Number(counter.innerHTML) + 1;
+  if (newCounterValue >= 10) {
+    counter.style.color = "red";
+  }
+  counter.innerHTML = newCounterValue;
+});
+decrementButton.addEventListener("click", function () {
+  console.log("- button clicked");
+  let newCounterValue = Number(counter.innerHTML) - 1;
+  if (newCounterValue < 10) {
+    counter.style.color = "black";
+  }
+  counter.innerHTML = newCounterValue;
+});
